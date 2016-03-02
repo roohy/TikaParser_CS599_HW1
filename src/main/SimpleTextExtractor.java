d0d45c40b28d002a76f8c06448cb57ed7aaa0c12
Ruhollah Shemirani <shemirani.r@gmail.com>
2016-03-01 19:55:14 -0800

    On the fly parsing

--- src/main/SimpleTextExtractor.java
+++ src/main/SimpleTextExtractor.java
@@ -8,6 +8,17 @@ import java.util.List;
 import org.apache.tika.Tika;
 import org.bson.Document;
 
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
@@ -19,8 +30,81 @@ public class SimpleTextExtractor {
 	static final String dumpDIR = "/home/roohy/PycharmProjects/CS599_HW1/dumped";
 	static final String mongoURL = "mongodb://admin:admin123@localhost:27017";
 	static final String databaseName = "CS599_HW1";
+	static final String POLAR = "polar-fulldump";
 	static final int maxSize = 50000;
 	
+	public static void main(String[] args) throws Exception{
+	
+		Tika tika = new Tika();
+		
+		MongoClientURI connectionString = new MongoClientURI(mongoURL);
+		MongoClient mongoClient = new MongoClient(connectionString);
+		MongoDatabase database = mongoClient.getDatabase(databaseName);
+		//database.createCollection("files");
+		MongoCollection<Document> collection = database.getCollection("files");
+		
+		int totalCount = 0;
+		AmazonS3 s3 = new AmazonS3Client();
+		//Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+        //s3.setRegion(usWest2);
+        /*
+        System.out.println("Listing buckets");
+        for (Bucket bucket : s3.listBuckets()) {
+            System.out.println(" - " + bucket.getName());
+        }*/
+        
+		
+        System.out.println("Listing objects");
+        ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
+                .withBucketName(POLAR));
+        S3Object object = null;
+        
+        while(true){
+        	for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                //System.out.println(" - " + objectSummary.getKey() + "  " +
+                  //      "(size = " + objectSummary.getSize() + ")");
+                object = s3.getObject(new GetObjectRequest(POLAR, objectSummary.getKey()));
+                Document doc = new Document("key",objectSummary.getKey());
+                S3ObjectInputStream inputStream = object.getObjectContent();
+                doc.append("type", tika.detect(inputStream));
+
+//                System.out.println("Content-Type: "  + doc.toJson());
+                collection.insertOne(doc);
+                inputStream.close();
+                totalCount++;
+            }
+        	
+        	if(objectListing.isTruncated()){
+        		objectListing = s3.listNextBatchOfObjects(objectListing);
+        	}
+        	else{
+        		break;
+        	}
+        }
+
+		System.out.print("number of documents is %d");
+		System.out.print(collection.count());
+        mongoClient.close();
+		
+	}
+	
+	public static void listf(String directoryName, ArrayList<File> files) {
+	    File directory = new File(directoryName);
+
+	    // get all the files from a directory
+	    File[] fList = directory.listFiles();
+	    for (File file : fList) {
+	        if (file.isFile()) {
+	            files.add(file);
+	        } else if (file.isDirectory()) {
+	            listf(file.getAbsolutePath(), files);
+	        }
+	    }
+	}
+}
+/*
+
+
 	public static void main(String[] args) throws Exception{
 	
 		Tika tika = new Tika();
@@ -33,7 +117,7 @@ public class SimpleTextExtractor {
 		/*for (String file : args){
 			String text = tika.parseToString(new File(file));
 			System.out.print(text);
-		}*/
+		}
 		//database.createCollection("files");
 		MongoCollection<Document> collection = database.getCollection("test");
 		
@@ -52,18 +136,6 @@ public class SimpleTextExtractor {
 		System.out.print(collection.count());
 		mongoClient.close();
 	}
-	
-	public static void listf(String directoryName, ArrayList<File> files) {
-	    File directory = new File(directoryName);
 
-	    // get all the files from a directory
-	    File[] fList = directory.listFiles();
-	    for (File file : fList) {
-	        if (file.isFile()) {
-	            files.add(file);
-	        } else if (file.isDirectory()) {
-	            listf(file.getAbsolutePath(), files);
-	        }
-	    }
-	}
-}
+
+*/
